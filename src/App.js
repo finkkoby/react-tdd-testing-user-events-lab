db2dfb9c9d4ad5576210ae20683ad0e6065b381e
Koby Fink <finkkoby@icloud.com>
2024-03-04 11:41:47 -0500

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [coding, setCoding] = useState(false)
+  const [painting, setPainting] = useState(false)
+  const [reading, setReading] = useState(false)
+  const [submit, setSubmit] = useState(false)
+
+  function handleNameChange(e) {
+    setName(e.target.value)
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value)
+  }
+
+  function handleCodingCheck() {
+    setCoding(!coding)
+  }
+
+  function handlePaintingCheck() {
+    setPainting(!painting)
+  }
+
+  function handleReadingCheck() {
+    setReading(!reading)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    setSubmit(true)
+  }
+
+  console.log(name, email)
+
   return (
     <main>
       <h1>Hi, I'm (your name)</h1>
@@ -18,6 +53,50 @@ function App() {
         <a href="https://github.com">GitHub</a>
         <a href="https://linkedin.com">LinkedIn</a>
       </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={handleNameChange}
+          id="name"
+        />
+        <label htmlFor="email">Email Address: </label>
+        <input
+          type="text"
+          placeholder="email address"
+          value={email}
+          onChange={handleEmailChange}
+          id="email"
+        />
+        <label>Select interests:</label>
+        <input
+          type='checkbox'
+          checked={coding}
+          id='coding'
+          onChange={handleCodingCheck}
+        />
+        <label htmlFor="coding">Coding</label>
+        <input
+          type='checkbox'
+          checked={painting}
+          id='painting'
+          onChange={handlePaintingCheck}
+        />
+        <label htmlFor="painting">Painting</label>
+        <input
+          type='checkbox'
+          checked={reading}
+          id='reading'
+          onChange={handleReadingCheck}
+        />
+        <label htmlFor="reading">Reading</label>
+        <button type='submit'>Submit</button>
+      </form>
+      {
+        submit ? <h3>Thank you for submitting your form!</h3> : null
+      }
     </main>
   );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event"
 
 import App from "../App";
 
@@ -66,26 +67,58 @@ test("displays the correct links", () => {
 
 // Newsletter Form - Initial State
 test("the form includes text inputs for name and email address", () => {
-  // your test code here
+  render(<App />)
+  expect(screen.getByPlaceholderText("name")).toBeInTheDocument()
+  expect(screen.getByPlaceholderText("email address")).toBeInTheDocument()
 });
 
 test("the form includes three checkboxes to select areas of interest", () => {
-  // your test code here
+  render(<App />)
+  expect(screen.getAllByRole("checkbox").length).toEqual(3)
 });
 
 test("the checkboxes are initially unchecked", () => {
-  // your test code here
+  render(<App />)
+  expect(screen.getByRole('checkbox', { name: /coding/i})).not.toBeChecked()
+  expect(screen.getByRole('checkbox', { name: /painting/i})).not.toBeChecked()
+  expect(screen.getByRole('checkbox', { name: /reading/i})).not.toBeChecked()
 });
 
 // Newsletter Form - Adding Responses
 test("the page shows information the user types into the name and email address form fields", () => {
-  // your test code here
+  render(<App />)
+  const nameInput = screen.getByPlaceholderText("name")
+  userEvent.type(nameInput, "my name")
+  expect(screen.getByDisplayValue("my name")).toBeInTheDocument()
+
+  const emailInput = screen.getByPlaceholderText("email address")
+  userEvent.type(emailInput, "myname@email.com")
+  expect(screen.getByDisplayValue("myname@email.com")).toBeInTheDocument()
 });
 
 test("checked status of checkboxes changes when user clicks them", () => {
-  // your test code here
+  render(<App />)
+
+  const codingCheck = screen.getByRole("checkbox", { name: /coding/i })
+  userEvent.click(codingCheck)
+  expect(codingCheck).toBeChecked()
+
+  const paintingCheck = screen.getByRole("checkbox", { name: /painting/i })
+  userEvent.click(paintingCheck)
+  expect(paintingCheck).toBeChecked()
+
+  const readingCheck = screen.getByRole("checkbox", { name: /reading/i })
+  userEvent.click(readingCheck)
+  expect(readingCheck).toBeChecked()
 });
 
 test("a message is displayed when the user clicks the Submit button", () => {
-  // your test code here
+  render(<App />)
+
+  const submit = screen.getByRole("button", { name: /submit/i })
+  userEvent.click(submit)
+  expect(screen.getByRole("heading", {
+    name: /thank you for submitting your form/i,
+    level: 3
+  }))
 });
